f9adecc900ba0f978660ca53aaf1bc1370b17f93
tgwittman <wittman.timothy@gmail.com>
2019-04-17 10:23:44 -0500

    Test created to test Photo Json serialization on Model

---
+++ PhotoAlbum.Tests/ModelsTests/PhotoTests.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using PhotoAlbum.Models;
+using Xunit;
+
+namespace PhotoAlbum.Tests.ModelsTests
+{
+    public class PhotoTests
+    {
+        [Fact]
+        public void When_NewPhoto_JsonSerializerPerformsCorrectly()
+        {
+            //Arrange
+            var photo = new Photo
+            {
+                Id = 1,
+                AlbumId = 1,
+                Title = "Title",
+                ThumbnailUrl = "https://via.placeholder.com/150/92bfbf",
+                Url = "https://via.placeholder.com/600/92bfbf"
+            };
+            string jsonstring = "{\n    \"albumId\": 1,\n    \"id\": 1,\n    \"title\": \"Title\",\n    \"url\": \"https://via.placeholder.com/600/92bfbf\",\n    \"thumbnailUrl\": \"https://via.placeholder.com/150/92bfbf\"\n  }";
+
+            //Act
+            var actual = new Photo();
+            var serializer = new DataContractJsonSerializer(typeof(Photo));
+            using (var test_stream = new MemoryStream(Encoding.UTF8.GetBytes(jsonstring)))
+            {
+                actual = serializer.ReadObject(test_stream) as Photo;
+            }
+
+            //Assert
+            Assert.Equal(photo.Id, actual.Id);
+            Assert.Equal(photo.AlbumId, actual.AlbumId);
+            Assert.Equal(photo.Title, actual.Title);
+            Assert.Equal(photo.ThumbnailUrl, actual.ThumbnailUrl);
+            Assert.Equal(photo.Url, actual.Url);
+        }
+    }
+}
